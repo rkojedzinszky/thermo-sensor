2def9162958ddafdc980fae03fd193eb57075ca9
Richard Kojedzinszky <krichy@cflinux.hu>
2016-01-16 21:56:56 +0100

    radio tuned
    - 38.3835 kBaud
    - GFSK
    - RX Filter BW 270.8 kHz
    - 30/32 bit sync word matching wo/CS
    - FEC off

--- common/include/radio.hpp
+++ common/include/radio.hpp
@@ -34,8 +34,9 @@ inline void Radio<cc1101>::setup_common()
 	cc1101::set(CC1101::FREQ0, 0xe1);
 
 	// modem configuration
-	cc1101::set(CC1101::MDMCFG2, 0x16);
-	cc1101::set(CC1101::MDMCFG1, 0xa2);
+	cc1101::set(CC1101::MDMCFG4, 0x6a);
+	cc1101::set(CC1101::MDMCFG3, 0x83);
+	cc1101::set(CC1101::MDMCFG2, 0x13);
 
 	// calibrate
 	cc1101::wcmd(CC1101::SCAL);
