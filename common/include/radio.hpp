a52204ea455e371b09e5d946b06e8c12a61dac4a
Richard Kojedzinszky <krichy@cflinux.hu>
2016-01-24 18:28:19 +0100

    radio tuned
    - msk
    - 115.2 kBaud
    - 10 dBm tx power
    - 270 kHz rx bw filter

--- common/include/radio.hpp
+++ common/include/radio.hpp
@@ -34,11 +34,10 @@ inline void Radio<cc1101>::setup_common()
 	cc1101::set(CC1101::FREQ0, 0xe1);
 
 	// modem configuration
-	cc1101::set(CC1101::MDMCFG4, 0xba);
-	cc1101::set(CC1101::MDMCFG3, 0x8e);
-	cc1101::set(CC1101::MDMCFG2, 0x13);
+	cc1101::set(CC1101::MDMCFG4, 0x6c);
+	cc1101::set(CC1101::MDMCFG2, 0x73);
 	cc1101::set(CC1101::MDMCFG1, 0xa2);
-	//cc1101::set(CC1101::DEVIATN, 0x47);
+	cc1101::set(CC1101::DEVIATN, 0x40);
 
 	// calibrate
 	cc1101::wcmd(CC1101::SCAL);
@@ -70,7 +69,7 @@ inline void Radio<cc1101>::setup_for_tx()
 	cc1101::set(CC1101::MCSM0, 0x38);
 
 	// PATABLE
-	cc1101::set(CC1101::PATABLE, 0x50);
+	cc1101::set(CC1101::PATABLE, 0xc0);
 
 	cc1101::release();
 }
