487739571bedadb5b862892cdeed5690f1af00a3
Richard Kojedzinszky <krichy@cflinux.hu>
2016-03-14 00:35:09 +0100

    sensor: sleep between 26-30 seconds

--- sensor/sensor_loop.cpp
+++ sensor/sensor_loop.cpp
@@ -3,7 +3,7 @@
 #include <interrupt/PCINT0.hpp>
 #include "sensor.hpp"
 
-static constexpr int delay_1 = 26;
+static constexpr int delay_1 = 24;
 static constexpr int delay_2 = 2;
 
 static constexpr unsigned int delay_1_ticks = delay_1 * delay_multiplier;
@@ -14,7 +14,7 @@ void Sensor::loop()
 	PCMSK = _BV(radio::USI::DI::pin);
 
 	for (;;) {
-		uint16_t ticks = delay_1_ticks + (lfsr.get() & 0x3f);
+		uint16_t ticks = delay_1_ticks + (lfsr.get() & 0x7f);
 		radio::select();
 		radio::set(CC1101::WOREVT1, ticks >> 8);
 		radio::set(CC1101::WOREVT0, ticks & 0xff);
