a1f150df67a598b59f9006485c52215cb528c21f
Richard Kojedzinszky <krichy@cflinux.hu>
2016-04-24 21:13:20 +0200

    sensor/pcbrel: pack source .pcb in zip

--- sensor/pcb/pcbrel.sh
+++ sensor/pcb/pcbrel.sh
@@ -9,7 +9,6 @@ rm -f "$ZIPFILE"
 sed -e "s/SENSOR_VERSION/$V/g" sensor.pcb > "$TMP/sensor.pcb"
 cd "$TMP"
 pcb -x gerber --gerberfile sensor sensor.pcb
-rm -f sensor.pcb
 zip -q9 "$ZIPFILE" sensor*
 cd /
 rm -rf "$TMP"
