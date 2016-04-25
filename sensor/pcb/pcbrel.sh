90e9a1e47c90f04f5c3f0fbbc241ac44eeb5eeb4
Richard Kojedzinszky <krichy@tvnetwork.hu>
2016-04-25 13:35:01 +0200

    sensor/pcb: version split into two lines

--- sensor/pcb/pcbrel.sh
+++ sensor/pcb/pcbrel.sh
@@ -2,11 +2,14 @@
 
 export LANG=C
 V=$(git describe --tags)
+V1=$(echo "$V" | cut -d- -f 1)
+V2=$(echo "$V" | cut -d- -f 2-)
+[ "$V1" = "$V2" ] && V2=""
 ZIPFILE="$(pwd)/sensor-$V.zip"
 TMP=$(mktemp -d)
 rm -f "$ZIPFILE"
 
-sed -e "s/SENSOR_VERSION/$V/g" sensor.pcb > "$TMP/sensor.pcb"
+sed -e "s/SENSOR_VER1/$V1/g" -e "s/SENSOR_VER2/$V2/g" sensor.pcb > "$TMP/sensor.pcb"
 cd "$TMP"
 pcb -x gerber --gerberfile sensor sensor.pcb
 zip -q9 "$ZIPFILE" sensor*
